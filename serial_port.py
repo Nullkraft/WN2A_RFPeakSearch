091eddc43f1ec35dc68c095bce44bf10c05e22af
Mark Stanley <mstanley@technologist.com>
2021-05-09 09:32:01 -0400

    Add small time delay after opening serial port.
    The Arduino requires a little time to be open before use. If the user is fast enough they could try to use the port before it is fully open. Then the program hangs in an infinite loop waiting for bytes that never arrive.

--- serial_port.py
+++ serial_port.py
@@ -2,6 +2,7 @@
 
 from serial.tools import list_ports
 import serial       # requires 'pip3 install pyserial'
+import time
 
 import sys
 line = lambda : sys._getframe(1).f_lineno
@@ -23,15 +24,15 @@ def set_speed(speed):
     if get_port() != None:
         ser = _open()
 
-def get_speed():
-    return _baud
-
 def set_port(selected_port):
     global ser
     global _port
     _port = selected_port
     if get_speed() != None:
         ser = _open()
+
+def get_speed():
+    return _baud
 
 def get_port():
     return _port
@@ -49,6 +50,8 @@ def list_os_ports():
 
 def _open():
     ser_port = serial.Serial(_port, _baud, timeout=100/int(_baud))
+    # Give the serial port a moment to open
+    time.sleep(0.5)
     print(name, line(), ':', ser_port.port, ser_port.baudrate)
     return ser_port
 

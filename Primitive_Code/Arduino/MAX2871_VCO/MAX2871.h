59453813b4ba47f4df7af940ef2f81e8c939780b
Mark Stanley <mstanley@technologist.com>
2021-05-17 15:36:41 -0400

    Arduino code: Change muxPinMode() function to use the mode parameter as the bitmask for modifying reg5 and reg2.
    It makes sense to use the mode value, which falls between 0x00 and 0xFF as the bit values that need to be changed in registers 5 and 2. This will make comparing the code with the spec sheet a one-to-one affair without having to analyze the bits.

---
+++ Primitive_Code/Arduino/MAX2871_VCO/MAX2871.h
@@ -0,0 +1,30 @@
+/*
+ * Hardware description of the MAX2871 Fractional/Integer-N Synthesizer/VCO chip made
+ * by Maxim Integrated.
+ */
+
+#ifndef __MAX2871.H__
+#define __MAX2871.H__
+
+/*
+ * MUX Configuration bits for setting the MUX MODE.
+ * Bit 3 ==> Reg 5 bit 18
+ * Bit 2 ==> Reg 2 bit 28
+ * Bit 1 ==> Reg 2 bit 27
+ * Bit 0 ==> Reg 2 bit 26
+ */
+#define MUX_TRI_STATE   0000  // Put MUX pin in tri-state output mode
+#define MUX_D_VDD       0001  // Set MUX pin to Vdd  (Use for troubleshooting comms)
+#define MUX_D_GND       0010  // Clear MUX pin Gnd   (Use for troubleshooting comms)
+#define MUX_R_DIV       0011  // R-divider output
+#define MUX_N_DIV       0100  // N-divider output/2
+#define MUX_ALOCK_DET   0101  // Analog lock detect
+#define MUX_DLOCK_DET   0110  // Digital lock detect
+#define MUX_SYNC        0111  // Sync Input
+//              1000 to 1011  // Reserved
+#define MUX_READ        1100  // Read SPI registers 06
+//              1101 to 1111  // Reserved
+
+
+
+#endif

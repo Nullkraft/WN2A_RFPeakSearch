d7869aac04c1765e7c2d6c60dff075ecc9d1555f
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2021-05-23 12:36:50 -0400

    Add parameter to _send_command() to simplify the code for the command_processor.

--- command_processor.py
+++ command_processor.py
@@ -1,21 +1,43 @@
 import serial_port as sp
 import sys
 
-# Utility to simplify print debugging. ycecream is a lot better, though.
+# Utility to simplify print debugging.
 line = lambda : sys._getframe(1).f_lineno
 name = __name__
 
-
-command = None
-
+# Arduino and MAX2871 Commands
 RF_Enable = b'E'
 RF_Disable = b'e'
 Arduino_LED_on = b'L'
 Arduino_LED_off = b'l'
 status_request = b'S'
 
-def _send_command():
-    global command
+
+# Currently will only work with RF_En pin status.
+# Add the Arduino LED pin status to improve usefulness
+def get_hw_status():
+    _send_command(status_request)
+    rf_out_status = sp.ser.read(1)
+    return rf_out_status
+
+
+def enable_rf_out():
+    _send_command(RF_Enable)
+
+
+def disable_rf_out():
+    _send_command(RF_Disable)
+
+
+def turn_Arduino_LED_on():
+    _send_command(Arduino_LED_on)
+
+
+def turn_Arduino_LED_off():
+    _send_command(Arduino_LED_off)
+
+
+def _send_command(command):
     msg = None
     if sp.ser.is_open:
         sp.ser.write(command)
@@ -25,34 +47,11 @@ def _send_command():
     return msg
 
 
-# Currently will only work with RF_En pin status.
-# Add the Arduino LED pin status to improve usefulness
-def get_hw_status():
-    global command
-    command = status_request
-    _send_command()
-    rf_out_status = sp.ser.read(1)
-    return rf_out_status
-
-def enable_rf_out():
-    global command
-    command = RF_Enable
-    _send_command()
 
 
-def disable_rf_out():
-    global command
-    command = RF_Disable
-    _send_command()
 
 
-def turn_Arduino_LED_on():
-    global command
-    command = Arduino_LED_on
-    _send_command()
 
 
-def turn_Arduino_LED_off():
-    global command
-    command = Arduino_LED_off
-    _send_command()
+
+

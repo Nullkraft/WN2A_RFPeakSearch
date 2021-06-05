6d57ac528d542e32a6d0025f2830846ae07e8b05
Mark Stanley <mstanley@technologist.com>
2021-06-05 12:32:51 -0400

    Rename config file read and write function names. Rename file name to config_fname. Improve comments.

--- serial_port.py
+++ serial_port.py
@@ -12,42 +12,47 @@ import os
 line = lambda : sys._getframe(1).f_lineno
 name = __name__
 
-file_name = 'serial.conf'
-ser = serial.Serial()      # Object that can be shared across source files.
-_baud = None     #
-_port = None
+
+# Set the default serial port name based on the user's platform.
+if sys.platform == "linux" or sys.platform == "linux2":
+    default_serial_port = '/dev/ttyUSB0'
+elif sys.platform == "darwin":
+    default_serial_port = '/dev/ttyUSB0'
+elif sys.platform == "win32":
+    default_serial_port = 'COM1'
+
+default_serial_speed = '9600'
+config_fname = 'serial.conf'
+
+# Serial port object that can be shared across source files.
+ser = serial.Serial()
+
+_baud = None    # Current port speed being used by the system.
+_port = None    # Current serial port being used by the system.
 
 
-# Provide a list of baudrates to populate the gui drop down.
+# Reports the list of baudrates available to this platform.
 def get_os_baudrates():
     return serial.Serial.BAUDRATES[12:] # Slice off anything slower than 9600
 
 
-def set_speed(speed):
+def set_speed(selected_speed):
     global _baud
-    read_serial_config()    # Preset _baud and _port from config file
-    _baud = speed           # Set a new _baud rate
-    _update_port()
+    read_config()    # Preset _baud and _port from config file
+    _baud = selected_speed  # Set a new _baud rate
+    _update_port()          # Open the port at the new speed and save it to the config
 
 
 def set_port(selected_port):
     global _port
-    read_serial_config()    # Preset _baud and _port from config file
+    read_config()    # Preset _baud and _port from config file
     _port = selected_port   # Set a new _port
-    _update_port()
+    _update_port()          # Open the new port and save it to the config
 
 
 def _update_port():
-    write_serial_config(_baud, _port)
-    port_open()     # fkn dependency! port_open() *must* come after write_serial_config()
-
-
-def get_speed():
-    return _baud
-
-
-def get_port():
-    return _port
+    write_config(_baud, _port)   # Saves new settings to the config file.
+    port_open()                         # Re-opens the port using the new config file settings.
 
 
 def list_os_ports():
@@ -62,13 +67,13 @@ def port_open():
     global ser
     global _baud
     global _port
-    global file_name
+    global config_fname
     ser_port = None
-    if not os.path.isfile(file_name):
-        write_serial_config()           # If needed create a default serial.conf
-    port_config = read_serial_config()  # Get the port we want from the config file.
-    active_ports = list_os_ports()      # List all the serial ports on this system.
-    if port_config[0] in active_ports:  # Find out if the port we want is actually alive.
+    if not os.path.isfile(config_fname):    # If needed create a new default serial.conf
+        write_config()
+    port_config = read_config()      # Get the port settings from the config file.
+    active_ports = list_os_ports()          # Get a list of all the serial ports on this system.
+    if port_config[0] in active_ports:      # If our port is alive on this system then open it.
         try:
             ser_port = serial.Serial(_port, _baud, timeout=100/int(_baud))
         except OSError as e:
@@ -76,50 +81,51 @@ def port_open():
                 print(name, line(), ": Another program has already opened the port. Arduino?")
     #        print(name, line(), ": OSError e =", e.errno, e.strerror)
         except TypeError:
-            print(name, line(), f': Missing or invalid key-value pair in {file_name}.')
-            write_serial_config()   # Someone messed up our serial.conf keys so create a default copy.
+            print(name, line(), f': Missing or invalid key-value pair in {config_fname}.')
+            write_config()   # Someone messed up our serial.conf keys so create a default copy.
         else:
-            time.sleep(0.2)        # The serial port needs a moment before calling ser_port.port, etc.
+            time.sleep(0.2)         # Give the port a moment to finish opening.
             print(name, line(), f': {ser_port.port} opened at {ser_port.baudrate} baud.')
         finally:
             ser = ser_port
 
 
-def write_serial_config(speed=9600, port='/dev/ttyUSB0'):
+def write_config(speed=default_serial_speed, port=default_serial_port):
+    global config_fname
     config = configparser.ConfigParser()
-    # Build the config file heirarchy and include some preset values
-    config['DEFAULT'] = {'port_speed': '9600', 'serial_port': '/dev/ttyUSB0'}
+    # Build the config file heirarchy.
+    config['DEFAULT'] = {}
+    config['DEFAULT']['port_speed'] = default_serial_speed
+    config['DEFAULT']['serial_port'] = default_serial_port
     config['Last.opened'] = {}
     config['Last.opened']['port_speed'] = str(speed)
     config['Last.opened']['serial_port'] = port
-
     try:
-        with open('serial.conf', 'w') as config_file:
+        with open(config_fname, 'w') as config_file:
             config.write(config_file)
     except PermissionError as err:
-        print(name, line(), f': Can\'t write to the file, {err.filename}')
+        print(name, line(), f': Can\'t write to {err.filename}')
     except AttributeError as err:
         print(name, line(), f': Unable to open one of the keys in {err.filename}')
 
 
-def read_serial_config():
+def read_config():
     global _port
     global _baud
-    global file_name
-    if os.path.isfile(file_name):
+    global config_fname
+    if os.path.isfile(config_fname):
         config = configparser.ConfigParser()
-        config.read(file_name)
+        config.read(config_fname)
         # Load port and speed from the serial.conf file.
         try:
             _port = config['Last.opened']['serial_port']
             _baud = config['Last.opened'].getint('port_speed')
         except KeyError as err:
-            print(name, line(), f': Missing key {err} from {file_name}.')
+            print(name, line(), f': Missing key {err} from {config_fname}.')
         else:
             return [_port, _baud]
-
     else:
-        print(name, line(), f': Unable to read {file_name}, file not found.')
+        print(name, line(), f': Unable to read {config_fname}, file not found.')
 
 
 

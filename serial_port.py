f8a70631d626235b866a9802884f7bc2cbb9149c
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2021-09-08 09:39:08 -0400

    Moved all serial_port.py functions into simple_serial class

--- serial_port.py
+++ serial_port.py
@@ -16,134 +16,137 @@ line = lambda : sys._getframe(1).f_lineno
 name = __name__
 
 
-
-# Set the default serial port name based on the user's platform.
-if sys.platform == "linux" or sys.platform == "linux2":
-    default_serial_port = '/dev/ttyUSB0'
-elif sys.platform == "darwin":
-    default_serial_port = '/dev/ttyUSB0'
-elif sys.platform == "win32":
-    default_serial_port = 'COM1'
-
-default_serial_speed = '9600'
-config_fname = 'serial.conf'
-
 # Serial port object that can be shared across source files.
 ser = serial.Serial()
 
-_baud = None    # Current port speed being used by the system.
-_port = None    # Current serial port being used by the system.
 
-
-# Reports the list of baudrates available to this platform.
-def get_os_baudrates():
-    return serial.Serial.BAUDRATES[12:] # Slice off anything slower than 9600
-
-
-def set_speed(selected_speed):
-    global _baud
-    read_config()    # Preset _baud and _port from config file
-    _baud = selected_speed  # Set a new _baud rate
-    _update_port()          # Open the port at the new speed and save it to the config
-
-
-def set_port(selected_port):
-    global _port
-    read_config()    # Preset _baud and _port from config file
-    _port = selected_port   # Set a new _port
-    _update_port()          # Open the new port and save it to the config
-
-
-def _update_port():
-    write_config(_baud, _port)   # Saves new settings to the config file.
-    port_open()                         # Re-opens the port using the new config file settings.
-
-
-def list_os_ports():
-    com_list = []
-    ports = list_ports.comports()
-    for port in ports:
-        com_list.append(port.device)
-    return com_list
-
-
-def port_open():
-    global ser
-    global _baud
-    global _port
-    global config_fname
-    ser_port = None
-    if not os.path.isfile(config_fname):    # If needed create a new default serial.conf
-        write_config()
-    port_config = read_config()      # Get the port settings from the config file.
-    active_ports = list_os_ports()          # Get a list of all the serial ports on this system.
-    if port_config[0] in active_ports:      # If our port is alive on this system then open it.
-        try:
-            ser_port = serial.Serial(_port, _baud, timeout=100/int(_baud))
-        except OSError as e:
-            if e.errno == error.EBUSY:
-                print(name, line(), ": Another program has already opened the port. Arduino?")
-    #        print(name, line(), ": OSError e =", e.errno, e.strerror)
-        except TypeError:
-            print(name, line(), f': Missing or invalid key-value pair in {config_fname}.')
-            write_config()   # Someone messed up our serial.conf keys so create a default copy.
-        else:
-            time.sleep(0.2)         # Give the port a moment to finish opening.
-            print(name, line(), f': {ser_port.port} opened at {ser_port.baudrate} baud.')
-        finally:
-            ser = ser_port
-
-
-def write_config(speed=default_serial_speed, port=default_serial_port):
-    global config_fname
-    config = configparser.ConfigParser()
-    # Build the config file heirarchy.
-    config['DEFAULT'] = {}
-    config['DEFAULT']['port_speed'] = default_serial_speed
-    config['DEFAULT']['serial_port'] = default_serial_port
-    config['Last.opened'] = {}
-    config['Last.opened']['port_speed'] = str(speed)
-    config['Last.opened']['serial_port'] = port
-    try:
-        with open(config_fname, 'w') as config_file:
-            config.write(config_file)
-    except PermissionError as err:
-        print(name, line(), f': Can\'t write to {err.filename}')
-    except AttributeError as err:
-        print(name, line(), f': Unable to open one of the keys in {err.filename}')
-
-
-def read_config():
-    global _port
-    global _baud
-    global config_fname
-    if os.path.isfile(config_fname):
-        config = configparser.ConfigParser()
-        config.read(config_fname)
-        # Load port and speed from the serial.conf file.
-        try:
-            _port = config['Last.opened']['serial_port']
-            _baud = config['Last.opened'].getint('port_speed')
-        except KeyError as err:
-            print(name, line(), f': Missing key {err} from {config_fname}.')
-        else:
-            return [_port, _baud]
-    else:
-        print(name, line(), f': Unable to read {config_fname}, file not found.')
-
-
-class serialWorker(QObject):
+class simple_serial(QObject):
     """
-    serialWorker will receive large amounts of data of a known size from the Arduino.
+    simple_serial will receive large amounts of data of a known size from the Arduino.
     """
+
+    default_serial_speed = '9600'
+#    default_serial_port = None
+    # Set the default serial port name based on the user's platform.
+    if sys.platform == "linux" or sys.platform == "linux2":
+        default_serial_port = '/dev/ttyUSB0'
+    elif sys.platform == "darwin":
+        default_serial_port = '/dev/ttyUSB0'
+    elif sys.platform == "win32":
+        default_serial_port = 'COM1'
+
+    _baud = None
+    _port = None
+    config_fname = 'serial.conf'
 
     finished = pyqtSignal(bytearray)
     progress = pyqtSignal(int)
 
     def __init__(self, parent=None):
         QObject.__init__(self, parent)
-        self.end_of_record = bytearray([255, 255])        # Arduino A2D is 10 bits so we can use 0xffff
-        self.ampl_data_bytes = bytearray()                # Store 8bit data from Arduino
+        self.end_of_record = bytearray([255, 255])  # Arduino A2D is only 10 bits so we can safely use 0xffff
+        self.ampl_data_bytes = bytearray()          # Store 8bit data from Arduino
+
+
+    def set_speed(self, selected_speed):
+        self._read_config()           # Preset _baud and _port from config file
+        self._baud = selected_speed  # Set a new _baud rate
+        self._update_port()          # Open the port at the new speed and save it to the config
+
+
+    def set_port(self, selected_port):
+        self._read_config()           # Preset _baud and _port from config file
+        self._port = selected_port   # Set a new _port
+        self._update_port()          # Open the new port and save it to the config
+
+
+    def get_os_ports(self):
+        """
+        Function get_os_ports()
+
+        @return List of available serial ports
+        @rtype List
+        """
+        com_list = []
+        ports = list_ports.comports()
+        for port in ports:
+            com_list.append(port.device)
+        return com_list
+
+
+    def get_os_baudrates(self):
+        """
+        Function get_os_baudrates()
+
+        @return Available serial rates, at or above 9600 baud, on your system.
+        @rtype Tuple
+        """
+        idx9600 = ser.BAUDRATES.index(9600)
+        return ser.BAUDRATES[idx9600:]      # Only show rates at or above 9600 baud
+
+
+    def _update_port(self):
+        self._write_config(self._baud, self._port)     # Saves new settings to the config file.
+        self.port_open()                    # This will reopen the last port used if it's available.
+
+
+    def port_open(self):
+        global ser
+        ser_port = None
+        if not os.path.isfile(self.config_fname):    # If needed create a new default serial.conf
+            self._write_config(self.default_serial_speed, self.default_serial_port)
+        port_config = self._read_config()            # Get the port settings from the config file.
+        active_ports = self.get_os_ports()           # Get a list of all the serial ports on this system.
+        if port_config[0] in active_ports:           # If our port is alive on this system then open it.
+            try:
+                ser_port = serial.Serial(self._port, self._baud, timeout=100/int(self._baud))
+            except OSError as e:
+                if e.errno == error.EBUSY:
+                    print(name, line(), ": Another program has already opened the port. Arduino?")
+        #        print(name, line(), ": OSError e =", e.errno, e.strerror)
+            except TypeError:
+                print(name, line(), f': One of the lines in {self.config_fname} is messed up.')
+                self._write_config(self.default_serial_speed, self.default_serial_port)     # Messed up serial.conf keys so recreate a default copy.
+            else:
+                time.sleep(0.2)         # Give the port a moment to finish opening.
+                print(name, line(), f': {ser_port.port} opened at {ser_port.baudrate} baud.')
+            finally:
+                ser = ser_port
+
+
+    def _write_config(self, speed=None, port=None):
+        config = configparser.ConfigParser()
+        # Build the config file heirarchy.
+        config['DEFAULT'] = {}
+        config['DEFAULT']['port_speed'] = self.default_serial_speed
+        config['DEFAULT']['serial_port'] = self.default_serial_port
+        config['Last.opened'] = {}
+        config['Last.opened']['port_speed'] = str(speed)
+        config['Last.opened']['serial_port'] = port
+        try:
+            with open(self.config_fname, 'w') as config_file:
+                config.write(config_file)
+        except PermissionError as err:
+            print(name, line(), f': Can\'t write to {err.filename}')
+        except AttributeError as err:
+            print(name, line(), f': Unable to open one of the keys in {err.filename}')
+
+
+    def _read_config(self):
+        if os.path.isfile(self.config_fname):
+            config = configparser.ConfigParser()
+            config.read(self.config_fname)
+            # Load port and speed from the serial.conf file.
+            try:
+                self._port = config['Last.opened']['serial_port']
+                self._baud = config['Last.opened'].getint('port_speed')
+            except KeyError as err:
+                print(name, line(), f': Missing key {err} from {self.config_fname}.')
+            else:
+                return [self._port, self._baud]
+        else:
+            print(name, line(), f': Unable to read {self.config_fname}, file not found.')
+
 
     def read_serial(self):
         """
@@ -157,13 +160,15 @@ class serialWorker(QObject):
                 break
         self.finished.emit(self.ampl_data_bytes)
 
-# End serialWorker() class
+# End simple_serial() class
 
 
 
 
 if __name__ == '__main__':
-    print(list_os_ports())
+    sw = simple_serial()
+    print(name, line(), f'Available serial ports = {sw.get_os_ports()}')
+    print(name, line(), f'Available baud rates = {sw.get_os_baudrates()}')
 
 
 

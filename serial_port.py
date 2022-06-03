2f954aaa361cdc5dbd68e532becb63c5db435926
Firstname Lastname <email_address>
2022-06-03 13:50:28 -0400

    Fixed problems with opening the last serial port that was used.

--- serial_port.py
+++ serial_port.py
@@ -20,41 +20,36 @@ ser = serial.Serial()
 
 class simple_serial(QObject):
     """
-    simple_serial is used to receive large amounts of data of a known size from the Arduino.
+    simple_serial is used to receive large amounts of data of a given bit width from the Arduino.
     """
-    default_serial_speed = '9600'
-    # Set the default serial port name based on the user's platform.
-    if sys.platform == "linux" or sys.platform == "linux2":
-        default_serial_port = '/dev/ttyUSB0'
-    elif sys.platform == "darwin":
-        default_serial_port = '/dev/ttyUSB0'
-    elif sys.platform == "win32":
-        default_serial_port = 'COM1'
-
-    _baud = None
-    _port = None
-    config_fname = 'serial.conf'
-
     finished = pyqtSignal(bytearray)
     progress = pyqtSignal(int)
+
 
     def __init__(self, parent=None):
         QObject.__init__(self, parent)
         self.end_of_record = bytearray([255, 255])  # Arduino A2D is only 10 bits so we can safely use 0xffff
-        self._read_config()          # Preset _baud and _port from config file
+        self.config_fname = 'serial.conf'
+        self.default_serial_speed = '9600'
+        # Set the default serial port name based on the user's platform.
+        if sys.platform == "linux" or sys.platform == "linux2":
+            self.default_serial_port = '/dev/ttyUSB0'
+        elif sys.platform == "darwin":
+            self.default_serial_port = '/dev/ttyUSB0'
+        elif sys.platform == "win32":
+            self.default_serial_port = 'COM1'
+
 
     def get_serial_port_list(self):
         """
         Function get_serial_port_list()
 
-        @return List of available serial ports from the local system.
+        @return List of available serial ports for the local system.
         @rtype List
         """
-        port_list = list()
-        ports = list_ports.comports()
-        for port in ports:
-            port_list.append(port.device)
+        port_list = [port.device for port in list_ports.comports()]
         return port_list
+
 
     def get_baud_rate_list(self):
         """
@@ -68,53 +63,47 @@ class simple_serial(QObject):
         baud_rate_list = baud_rate_list[idx_9600_baud:] # Limit baud rate list to speeds starting from 9600 baud
         return baud_rate_list                     
 
-    def set_serial_speed(self, selected_speed):
-        self._baud = int(selected_speed)    # Set a new _baud rate
 
-    def set_serial_port(self, selected_port):
-        self._port = selected_port          # Set a new _port
-
-    def port_open(self):
+    def port_open(self, baud_rate=None, port=None):
         global ser
-        ser_port = None
+        self.port = port
+        self.baud = baud_rate
+        # Check for serial config file and create a new one, using default values, if it's missing
         if not os.path.isfile(self.config_fname):    # If needed create a new default serial.conf
             self._write_config(self.default_serial_speed, self.default_serial_port)
-        config_port = self._read_config()            # Get the port settings from the config file.
-        active_ports = self.get_serial_port_list()   # Get a list of all the serial ports on this system.
-        if config_port[0] in active_ports:
-            selected_port = config_port[0]
-        else:
-            selected_port = active_ports[0]
-            print(name, line(), f'selected active_port = {selected_port}')
-
-        if selected_port != None:           # If our port is alive on this system then open it.
+        # Get the port settings from the config file.
+        last_open_port = self.read_config()
+        if self.port is None:
+            self.port = last_open_port[0]
+        if self.baud is None:
+            self.baud = last_open_port[1]
+        # Get the list of all serial ports on this system.
+        active_ports_list = self.get_serial_port_list()
+        if self.port in active_ports_list:
             try:
-                ser_port = serial.Serial(selected_port, self._baud, timeout=100/int(self._baud))
-                self._port = selected_port
+                ser_port = serial.Serial(self.port, self.baud, timeout=100/int(self.baud))
             except OSError as e:
                 if e.errno == error.EBUSY:
-                    print(name, line(), f': {selected_port} has already been opened by another program. Arduino?')
-        #        print(name, line(), ": OSError e =", e.errno, e.strerror)
+                    print(name, line(), f': {self.port} has already been opened by another program. Arduino?')
             except TypeError:
-                print(name, line(), f': One of the lines in {self.config_fname} is messed up.')
+                print(name, line(), f': {self.config_fname} is corrupt. New default file created.')
                 self._write_config(self.default_serial_speed, self.default_serial_port)     # Corrupted file contents so recreate a default.
             else:
                 time.sleep(0.2)                                 # Give the port a moment to finish opening.
                 print(name, line(), f'{ser_port.port} opened at {ser_port.baudrate} baud.')
             finally:
                 ser = ser_port
+                self._write_config(current_speed=self.baud, current_port=self.port)
         else:
-            print(name, line(), f'config_port[0], {config_port[0]}, is NOT in active_ports')
+            print(name, line(), 'No serial ports found')
 
-    def _write_config(self, speed=None, port=None):
+
+    def _write_config(self, current_speed=None, current_port=None):
         config = configparser.ConfigParser()
         # Build the config file heirarchy.
-        config['DEFAULT'] = {}
-        config['DEFAULT']['port_speed'] = self.default_serial_speed
-        config['DEFAULT']['serial_port'] = self.default_serial_port
         config['Last.opened'] = {}
-        config['Last.opened']['port_speed'] = str(speed)
-        config['Last.opened']['serial_port'] = port
+        config['Last.opened']['port_speed'] = str(current_speed)
+        config['Last.opened']['serial_port'] = current_port
         try:
             with open(self.config_fname, 'w') as config_file:
                 config.write(config_file)
@@ -123,25 +112,23 @@ class simple_serial(QObject):
         except AttributeError as err:
             print(name, line(), f': Unable to open one of the keys in {err.filename}')
 
-    def _read_config(self):
+
+    def read_config(self):
         if os.path.isfile(self.config_fname):
             config = configparser.ConfigParser()
             config.read(self.config_fname)
             # Load port and speed from the serial.conf file.
             try:
-                self._port = config['Last.opened']['serial_port']
-                self._baud = config['Last.opened'].getint('port_speed')
+                port = config['Last.opened']['serial_port']
+                baud = config['Last.opened'].getint('port_speed')
             except KeyError as err:
                 print(name, line(), f': Missing key {err} from {self.config_fname}.')
             else:
-                return [self._port, self._baud]
+                return [port, baud]
         else:
             print(name, line(), f': Unable to read {self.config_fname}, file not found.')
 
-    def save_settings(self):
-        self._write_config(self._baud, self._port)
-        print(name, line(), f'Supposedly saved _baud = {self._baud} and _port = {self._port}')
-        
+
     def read_serial(self):
         """
         Worker thread for collecting incoming serial data
@@ -163,6 +150,7 @@ class simple_serial(QObject):
 if __name__ == '__main__':
     print("")
     print(simple_serial().get_baud_rate_list())
+    print(simple_serial().get_serial_port_list())
 
 
 

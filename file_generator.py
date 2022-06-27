5009093f980a9e8dfdfed22b08b45d89ae6e509d
Firstname Lastname <email_address>
2022-06-27 11:16:18 -0400

    Continue work on the tools to generate initial files used for the calibration process.

--- file_generator.py
+++ file_generator.py
@@ -1,9 +1,8 @@
 import psutil
-from multiprocessing import Pool as pool
 import numpy as np
-import time
 import sys
 import spectrumAnalyzer as sa
+from hardware_cfg import cfg
 
 # Utilities provided for print debugging.
 line = lambda: f'line {str(sys._getframe(1).f_lineno)},'
@@ -16,74 +15,59 @@ if __name__ == '__main__':
     print(f'Number of physical CPUs = {cpu_hw_cores}')
     num_loops = 1
 
-    ref_list = [66.000, 66.666]
-    ref_divider = 2
-    IF1 = 3600                      # First intermediate frequency in MHz
-    IF2 = 315                       # Second intermediate frequency in MHz
-    RFin_start = 0.0
-    RFin_stop = 3000.001            # Spectrum analyzer bandwidth is 3 GHz
-    RFin_step = 0.001
-    
     """ See if you can replace these two functions with a lambda  """
     def ref1_mhz_to_fmn(LO2_target_freq):
-        fmn = sa.MHz_to_fmn(LO2_target_freq, Fref=66.0)
-        return fmn
+        fmn_list = sa.MHz_to_fmn(LO2_target_freq, Fref=66.0)
+        return fmn_list
 
     def ref2_mhz_to_fmn(LO2_target_freq):
-        fmn = sa.MHz_to_fmn(LO2_target_freq, Fref=66.666)
-        return fmn
+        fmn_list = sa.MHz_to_fmn(LO2_target_freq, Fref=66.666)
+        return fmn_list
 
-    start = time.perf_counter()
-
-    # Fpfd is the LO1 step size
-    Fpfd_list = [Fpfd/ref_divider for Fpfd in ref_list]
+    # Fpfd sets the LO1 step size. 'ref_divider' is R and can be 1 for any RFin above 46.7 MHz
+    Fpfd_list = [Fpfd/cfg.ref_divider for Fpfd in cfg.ref_list]
     # RFin_list contains every frequency from 0 to 3000.001 MHz in 1 kHz steps
-    RFin_list = [freq for freq in np.arange(RFin_start, RFin_stop, RFin_step)]
+    RFin_list = [freq for freq in np.arange(cfg._RFin_start, cfg._RFin_stop, cfg._RFin_step)]
+    # Create the list of LO1 frequencies when using reference clock 1.
+    LO1_ref1_freq_list = [int((cfg.IF1+RFin)/Fpfd_list[0]) * Fpfd_list[0] for RFin in RFin_list]
+    LO1_ref1_freq_list = [round(x, 9) for x in LO1_ref1_freq_list]
+    # Create the list of LO1 frequencies when using reference clock 2.
+    LO1_ref2_freq_list = [int((cfg.IF1+RFin)/Fpfd_list[1]) * Fpfd_list[1] for RFin in RFin_list]
+    LO1_ref2_freq_list = [round(x, 9) for x in LO1_ref2_freq_list]
+    # Create the list of LO1 N values for setting the frequency of the ADF4356 chip when using reference clock 1
+    LO1_ref1_N_list = [int(n/Fpfd_list[0]) for n in LO1_ref1_freq_list]
+    # Create the list of LO1 N values for setting the frequency of the ADF4356 chip when using reference clock 2
+    LO1_ref2_N_list = [int(n/Fpfd_list[1]) for n in LO1_ref2_freq_list]
+    # Create the frequency lookup tables for LO1
+    LO1_ref1_freq_dict = dict(zip(RFin_list, LO1_ref1_freq_list))
+    LO1_ref2_freq_dict = dict(zip(RFin_list, LO1_ref2_freq_list))
+    # Create the frequency lookup tables for LO2. (LO2_freq = LO1 - RFin + cfg.IF2)
+    LO2_ref1_freq_list = [LO1_ref1_freq_dict[RFin] - RFin + cfg.IF2 for RFin in RFin_list]
+    LO2_ref2_freq_list = [LO1_ref2_freq_dict[RFin] - RFin + cfg.IF2 for RFin in RFin_list]
+    # Create the LO2 control codes for setting the frequency of the MAX2871 chip for ref clocks 1 and 2
+    LO2_ref1_fmn_list = map(ref1_mhz_to_fmn, LO2_ref1_freq_list)
+    LO2_ref2_fmn_list = map(ref2_mhz_to_fmn, LO2_ref2_freq_list)
 
-    # List of LO1 frequencies when using reference clock 1.
-    LO1_ref1_list = [int((IF1+RFin)/Fpfd_list[0]) * Fpfd_list[0] for RFin in RFin_list]
-    # List of LO1 frequencies when using reference clock 2.
-    LO1_ref2_list = [int((IF1+RFin)/Fpfd_list[1]) * Fpfd_list[1] for RFin in RFin_list]
-    # Save LO1 frequency files for ref1 and ref2
+    # Save LO1 files for ref1 and ref2
     with open("LO1_ref1_freq_steps.csv", 'w') as f:
-        [f.write(str(freq) + '\n') for freq in LO1_ref1_list]
+        [f.write(str(freq) + '\n') for freq in LO1_ref1_freq_list]
     with open("LO1_ref2_freq_steps.csv", 'w') as f:
-        [f.write(str(freq) + '\n') for freq in LO1_ref2_list]
-
-    # List of LO1 N values for setting the frequency of the ADF435 chip when using reference clock 1
-    LO1_ref1_N_list = [int(n/Fpfd_list[0]) for n in LO1_ref1_list]
-    # List of LO1 N values for setting the frequency of the ADF435 chip when using reference clock 2
-    LO1_ref2_N_list = [int(n/Fpfd_list[1]) for n in LO1_ref2_list]
+        [f.write(str(freq) + '\n') for freq in LO1_ref2_freq_list]
     with open("LO1_ref1_N_steps.csv", 'w') as f:
         [f.write(str(N) + '\n') for N in LO1_ref1_N_list]
     with open("LO1_ref2_N_steps.csv", 'w') as f:
         [f.write(str(N) + '\n') for N in LO1_ref2_N_list]
-
-    LO1_ref1_dict = dict(zip(RFin_list, LO1_ref1_list))
-    LO1_ref2_dict = dict(zip(RFin_list, LO1_ref2_list))
-
-    # LO2 = LO1 - RFin + IF2
-    LO2_ref1_list = [LO1_ref1_dict[RFin] - RFin + IF2 for RFin in RFin_list]
-    LO2_ref2_list = [LO1_ref2_dict[RFin] - RFin + IF2 for RFin in RFin_list]
+    # Save LO2 files for ref1 and ref2
     with open("LO2_ref1_freq_steps.csv", 'w') as f:
-        [f.write(str(round(freq, 9)) + '\n') for freq in LO2_ref1_list]
+        [f.write(str(round(freq, 9)) + '\n') for freq in LO2_ref1_freq_list]
     with open("LO2_ref2_freq_steps.csv", 'w') as f:
-        [f.write(str(round(freq, 9)) + '\n') for freq in LO2_ref2_list]
-
-    with pool(cpu_hw_cores) as executor:
-        LO2_ref1_fmn_list = executor.map(ref1_mhz_to_fmn, LO2_ref1_list)
-        LO2_ref2_fmn_list = executor.map(ref2_mhz_to_fmn, LO2_ref2_list)
+        [f.write(str(round(freq, 9)) + '\n') for freq in LO2_ref2_freq_list]
     with open("LO2_ref1_fmn_steps.csv", 'w') as f:
         [f.write(str(fmn) + '\n') for fmn in LO2_ref1_fmn_list]
     with open("LO2_ref2_fmn_steps.csv", 'w') as f:
-        [f.write(str(fmn) + '\n') for fmn in LO2_ref1_fmn_list]
+        [f.write(str(fmn) + '\n') for fmn in LO2_ref2_fmn_list]
 
-    stop = time.perf_counter()
-
-    print(f'Time to create files = {round((stop - start)/60, 2)} min(s)')
     print("Done")
-
-
 
 
 

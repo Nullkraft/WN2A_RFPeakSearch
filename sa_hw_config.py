66b1c203b6ca522bd58d0ee82cb6afeee2c9e26c
Firstname Lastname <email_address>
2022-07-02 10:09:39 -0400

    Remove functions that will be replaced with a lookup table

--- sa_hw_config.py
+++ sa_hw_config.py
@@ -36,32 +36,9 @@ class sa_hw_config():
                 f.write(LO1_freq_list = executor.map(specan.MHz_to_fmn, LO1_list))
 
     
-    def create_LO1_freq_step_file(self, in_file: str='SA_full_range_1kHz_steps.csv', out_file: str='LO1_dict_keys.csv'):
-        """
-        Public method
-        Create the config file that contains every value of LO1 frequency from
-        3000-to-6000 MHz using the SA_full_range_freq_steps as input.
-        """
-        with open(in_file, 'r') as in_freq:
-            print(in_freq)
-#        RF_in = 1700.0
-#        LO1_freq_lo_ref = specan.get_LO1_freq(RFin=RF_in, Fref_MHz=66.000, IF1_MHz=3600.0)
-#        LO1_freq_hi_ref = specan.get_LO1_freq(RFin=RF_in, Fref_MHz=66.666, IF1_MHz=3600.0)
-#        return LO1_freq_lo_ref,  LO1_freq_hi_ref
-
 
 if __name__ == '__main__':
     print()
-#    RF_in = 1700.0
-#    low_ref = 33.000        # MHz
-#    hi_ref = 33.333         # MHz
-    sahw = sa_hw_config
-#    LO1_low, LO1_hi = sa_hw_config.create_LO1_freq_step_file(sahw)
-#    IF1_low = np.around(LO1_low - RF_in, 6)
-#    IF1_hi = np.around(LO1_hi - RF_in, 6)
-#    print(f'LO1_low = {LO1_low}   MHz;\tIF1_low = {IF1_low};\tIF1_low % low_ref = {np.around(IF1_low % low_ref, 6)}')
-#    print(f'LO1_hi  = {LO1_hi} MHz;\tIF1_hi  = {IF1_hi};\tIF1_hi % hi_ref   = {np.around(IF1_hi % hi_ref, 6)}')
-    LO1_list = sa_hw_config.create_SA_freq_step_file(sahw)
 
 
 

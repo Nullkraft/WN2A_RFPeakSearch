b1fcd97252b937b8d533dd42b95daf2880ff6c8b
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2022-07-06 14:44:32 -0400

    Convert file_generator script to a class

--- file_generator.py
+++ file_generator.py
@@ -7,62 +7,77 @@ import numpy as np
 import spectrumAnalyzer as sa
 from hardware_cfg import cfg
 
-def ref1_mhz_to_fmn(LO2_target_freq):
-    fmn = sa.MHz_to_fmn(LO2_target_freq, cfg.ref_clock_tuple[0])
-    return fmn
+class data_generator():
+    # Fpfd sets the LO1 step size where ref_divider=1 for LO1 frequencies >= 3000 MHz
+    fpfd1 = cfg.Fpfd1
+    fpfd2 = cfg.Fpfd2
 
-def ref2_mhz_to_fmn(LO2_target_freq):
-    fmn = sa.MHz_to_fmn(LO2_target_freq, cfg.ref_clock_tuple[1])
-    return fmn
+    def ref1_mhz_to_fmn(self, LO2_target_freq):
+        fmn = sa.MHz_to_fmn(LO2_target_freq, cfg.ref_clock_tuple[0])
+        return fmn
+
+    def ref2_mhz_to_fmn(self, LO2_target_freq):
+        fmn = sa.MHz_to_fmn(LO2_target_freq, cfg.ref_clock_tuple[1])
+        return fmn
+
+    def create_data(self):
+        # RFin_array contains every frequency from 0 to 3000.001 MHz in 1 kHz steps
+        RFin_array = np.arange(cfg._RFin_start, cfg._RFin_stop, cfg._RFin_step)
+        # Create the list of LO1 frequencies when using reference clock 1.
+        self.LO1_ref1_freq_list = [int((cfg.IF1 + freq) / self.fpfd1) * self.fpfd1 for freq in RFin_array]
+        self.LO1_ref1_freq_list = [round(x, 9) for x in self.LO1_ref1_freq_list]
+        # Create the list of LO1 frequencies when using reference clock 2.
+        self.LO1_ref2_freq_list = [int((cfg.IF1 + freq) / self.fpfd2) * self.fpfd2 for freq in RFin_array]
+        self.LO1_ref2_freq_list = [round(x, 9) for x in self.LO1_ref2_freq_list]
+        # Create the list of LO1 N values for setting the frequency of the ADF4356 chip when using reference clock 1
+        self.LO1_ref1_N_list = [int(n/self.fpfd1) for n in self.LO1_ref1_freq_list]
+        # Create the list of LO1 N values for setting the frequency of the ADF4356 chip when using reference clock 2
+        self.LO1_ref2_N_list = [int(n/self.fpfd2) for n in self.LO1_ref2_freq_list]
+        # Create the frequency lookup tables for LO1
+        self.LO1_ref1_freq_dict = dict(zip(RFin_array, self.LO1_ref1_freq_list))
+        self.LO1_ref2_freq_dict = dict(zip(RFin_array, self.LO1_ref2_freq_list))
+        # Create the frequency lookup tables for LO2. (LO2_freq = LO1 - freq + cfg.IF2)
+        self.LO2_ref1_freq_list = [self.LO1_ref1_freq_dict[freq] - freq + cfg.IF2 for freq in RFin_array]
+        self.LO2_ref2_freq_list = [self.LO1_ref2_freq_dict[freq] - freq + cfg.IF2 for freq in RFin_array]
+        # Create the LO2 control codes for setting the frequency of the MAX2871 chip for ref clocks 1 and 2
+        self.LO2_ref1_fmn_list = map(self.ref1_mhz_to_fmn, self.LO2_ref1_freq_list)
+        self.LO2_ref2_fmn_list = map(self.ref2_mhz_to_fmn, self.LO2_ref2_freq_list)
+
+    def save_data_files(self):
+        """
+        Save LO1 files for ref1 and ref2
+        """
+        with open("LO1_ref1_freq_steps.csv", 'w') as f:
+            [f.write(str(freq) + '\n') for freq in self.LO1_ref1_freq_list]
+        with open("LO1_ref2_freq_steps.csv", 'w') as f:
+            [f.write(str(freq) + '\n') for freq in self.LO1_ref2_freq_list]
+        with open("LO1_ref1_N_steps.csv", 'w') as f:
+            [f.write(str(N) + '\n') for N in self.LO1_ref1_N_list]
+        with open("LO1_ref2_N_steps.csv", 'w') as f:
+            [f.write(str(N) + '\n') for N in self.LO1_ref2_N_list]
+        """
+        Save LO2 files for ref1 and ref2
+        """
+        with open("LO2_ref1_freq_steps.csv", 'w') as f:
+            [f.write(str(round(freq, 9)) + '\n') for freq in self.LO2_ref1_freq_list]
+        with open("LO2_ref2_freq_steps.csv", 'w') as f:
+            [f.write(str(round(freq, 9)) + '\n') for freq in self.LO2_ref2_freq_list]
+        with open("LO2_ref1_fmn_steps.csv", 'w') as f:
+            [f.write(str(fmn) + '\n') for fmn in self.LO2_ref1_fmn_list]
+        with open("LO2_ref2_fmn_steps.csv", 'w') as f:
+            [f.write(str(fmn) + '\n') for fmn in self.LO2_ref2_fmn_list]
 
 if __name__ == '__main__':
     print()
 
-    # Fpfd sets the LO1 step size where ref_divider=1 for LO1 frequencies >= 3000 MHz
-    Fpfd1 = cfg.ref_clock_tuple[0] / cfg.ref_divider
-    Fpfd2 = cfg.ref_clock_tuple[1] / cfg.ref_divider
-    # RFin_array contains every frequency from 0 to 3000.001 MHz in 1 kHz steps
-    RFin_array = np.arange(cfg._RFin_start, cfg._RFin_stop, cfg._RFin_step)
-    # Create the list of LO1 frequencies when using reference clock 1.
-    LO1_ref1_freq_list = [int((cfg.IF1 + freq) / Fpfd1) * Fpfd1 for freq in RFin_array]
-    LO1_ref1_freq_list = [round(x, 9) for x in LO1_ref1_freq_list]
-    # Create the list of LO1 frequencies when using reference clock 2.
-    LO1_ref2_freq_list = [int((cfg.IF1 + freq) / Fpfd2) * Fpfd2 for freq in RFin_array]
-    LO1_ref2_freq_list = [round(x, 9) for x in LO1_ref2_freq_list]
-    # Create the list of LO1 N values for setting the frequency of the ADF4356 chip when using reference clock 1
-    LO1_ref1_N_list = [int(n/Fpfd1) for n in LO1_ref1_freq_list]
-    # Create the list of LO1 N values for setting the frequency of the ADF4356 chip when using reference clock 2
-    LO1_ref2_N_list = [int(n/Fpfd2) for n in LO1_ref2_freq_list]
-    # Create the frequency lookup tables for LO1
-    LO1_ref1_freq_dict = dict(zip(RFin_array, LO1_ref1_freq_list))
-    LO1_ref2_freq_dict = dict(zip(RFin_array, LO1_ref2_freq_list))
-    # Create the frequency lookup tables for LO2. (LO2_freq = LO1 - freq + cfg.IF2)
-    LO2_ref1_freq_list = [LO1_ref1_freq_dict[freq] - freq + cfg.IF2 for freq in RFin_array]
-    LO2_ref2_freq_list = [LO1_ref2_freq_dict[freq] - freq + cfg.IF2 for freq in RFin_array]
-    # Create the LO2 control codes for setting the frequency of the MAX2871 chip for ref clocks 1 and 2
-    LO2_ref1_fmn_list = map(ref1_mhz_to_fmn, LO2_ref1_freq_list)
-    LO2_ref2_fmn_list = map(ref2_mhz_to_fmn, LO2_ref2_freq_list)
+    dg = data_generator()
+    print(f'Fpfd values 1 & 2 are {dg.fpfd1} & {dg.fpfd2}')
+    
+    dg.create_data()
+    dg.save_data_files()
 
-    # Save LO1 files for ref1 and ref2
-    with open("LO1_ref1_freq_steps.csv", 'w') as f:
-        [f.write(str(freq) + '\n') for freq in LO1_ref1_freq_list]
-    with open("LO1_ref2_freq_steps.csv", 'w') as f:
-        [f.write(str(freq) + '\n') for freq in LO1_ref2_freq_list]
-    with open("LO1_ref1_N_steps.csv", 'w') as f:
-        [f.write(str(N) + '\n') for N in LO1_ref1_N_list]
-    with open("LO1_ref2_N_steps.csv", 'w') as f:
-        [f.write(str(N) + '\n') for N in LO1_ref2_N_list]
-    # Save LO2 files for ref1 and ref2
-    with open("LO2_ref1_freq_steps.csv", 'w') as f:
-        [f.write(str(round(freq, 9)) + '\n') for freq in LO2_ref1_freq_list]
-    with open("LO2_ref2_freq_steps.csv", 'w') as f:
-        [f.write(str(round(freq, 9)) + '\n') for freq in LO2_ref2_freq_list]
-    with open("LO2_ref1_fmn_steps.csv", 'w') as f:
-        [f.write(str(fmn) + '\n') for fmn in LO2_ref1_fmn_list]
-    with open("LO2_ref2_fmn_steps.csv", 'w') as f:
-        [f.write(str(fmn) + '\n') for fmn in LO2_ref2_fmn_list]
 
-    print("Done")
+    print("Generator done")
 
 
 
--- hardware_cfg.py
+++ hardware_cfg.py
@@ -11,6 +11,10 @@ RFin_list = list()         # List of every kHz step from 0 to 3,000,000 kHz
 class cfg():
     ref_clock_tuple: float = 66.000, 66.666 # Ref_1, Ref_2
     ref_divider: int = 1                    # R from the MAX2871 spec sheet
+
+    Fpfd1 = ref_clock_tuple[0] / ref_divider
+    Fpfd2 = ref_clock_tuple[1] / ref_divider
+
     IF1: float = 3600.0                     # First intermediate frequency in MHz
     IF2: float = 315.0                      # Second intermediate frequency in MHz
 

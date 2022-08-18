57d5831dadad46ca31764bf2bab73d64d09c17c8
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2022-08-18 08:14:05 -0400

    Remove comment lines of code

--- calibrate_sa.py
+++ calibrate_sa.py
@@ -78,8 +78,7 @@ class Calibrate():
 
     """
 
-
-    def __init__(self, fname):
+    def __init__(self, fname: str) -> None:
         self.calibrated_dict = {}
         pass
 
@@ -98,37 +97,8 @@ class Calibrate():
         return self.calibrated_dict
 
 
-
-""" ********************* This block will move to spectrumAnalyzer.py ************************* """
-#LO1_freq = self.load_list(('LO1_ref1_freq_steps.csv', float))  # For plotting. Convert f from a string to float
-#LO2_freq = self.load_list(('LO2_ref1_freq_steps.csv', float))  # For plotting. Convert freq from string to float
-#LO1_freq = self.load_list(('LO1_ref2_freq_steps.csv', float))  # For plotting. Convert f from a string to float
-#LO2_freq = self.load_list(('LO2_ref2_freq_steps.csv', float))  # For plotting. Convert freq from string to float
-"""
-Reading the sweep dictionary from a file
-"""
-#test_dict = read_dict('full_control_ref1.csv')
-"""
-Set the sweep range and sweep step size
-"""
-#user_step_dict = dict(list(full_sweep_step_dict.items())[idx_start:idx_stop:idx_step])
-
-""" ********************************************************************************************* """
-
 if __name__ == '__main__':
     print()
 
-    
+
     print('Calibraion complete')
-
-
-
-
-
-
-
-
-
-
-
-

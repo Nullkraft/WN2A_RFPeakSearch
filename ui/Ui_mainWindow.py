4c349da19a61bc062f3781258d5fb3d9077845c5
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2022-08-21 10:26:10 -0400

    Add GPL V3 license header

--- ui/Ui_mainWindow.py
+++ ui/Ui_mainWindow.py
@@ -1,22 +1,3 @@
-#
-# This file is part of WN2A_RFPeakSearch.
-# 
-# This file is part of the WN2A_RFPeakSearch distribution (https://github.com/Nullkraft/WN2A_RFPeakSearch).
-# Copyright (c) 2021 Mark Stanley.
-# 
-# WN2A_RFPeakSearch is free software: you can redistribute it and/or modify  
-# it under the terms of the GNU General Public License as published by  
-# the Free Software Foundation, version 3.
-#
-# WN2A_RFPeakSearch is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License 
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-
 # Form implementation generated from reading ui file '/home/mark/projects/python/WN2A_RFPeakSearch/ui/mainWindow.ui'
 #
 # Created by: PyQt6 UI code generator 6.3.1

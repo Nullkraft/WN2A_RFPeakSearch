182209c36d9390b4bb7cf84928fcf07ac7ce1b9b
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2023-03-27 19:32:12 -0400

    Display numbers using engineering notation

--- engineering_notation.py
+++ engineering_notation.py
@@ -1,31 +1,31 @@
 
 
-class engineering_notation():
+class eng_notation():
     def init(self):
         pass
         
-    def eng_format(self, x):
+    def eng_format(self, x, units: str=''):
         if x >= 1e12:
-            return '{:.2f}T'.format(x / 1e12)
+            return '{:.2f}e12 '.format(x / 1e12) + units
         elif x >= 1e9:
-            return '{:.2f}G'.format(x / 1e9)
+            return '{:.2f}e9 '.format(x / 1e9) + units
         elif x >= 1e6:
-            return '{:.2f}M'.format(x / 1e6)
+            return '{:.2f}e6 '.format(x / 1e6) + units
         elif x >= 1e3:
-            return '{:.2f}k'.format(x / 1e3)
+            return '{:.2f}e3 '.format(x / 1e3) + units
         elif x >= 1e0:
-            return '{:.2f}'.format(x)
+            return '{:.2f} '.format(x) + units
         elif x >= 1e-3:
-            return '{:.2f}m'.format(x / 1e-3)
+            return '{:.2f}e-3 '.format(x / 1e-3) + units
         elif x >= 1e-6:
-            return '{:.2f}u'.format(x / 1e-6)
+            return '{:.2f}e-6 '.format(x / 1e-6) + units
         elif x >= 1e-9:
-            return '{:.2f}n'.format(x / 1e-9)
+            return '{:.2f}e-9 '.format(x / 1e-9) + units
         else:
-            return '{:.2f}p'.format(x / 1e-12)
+            return '{:.2f}e-12 '.format(x / 1e-12) + units
 
 if __name__ == '__main__':
-    en = engineering_notation()
+    en = eng_notation()
     test_x = 93e-5 * 1e-2
     formatted_x = en.eng_format(test_x)
     print(formatted_x)

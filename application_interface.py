14c5620ef50720dc407dbf0a3a8858050c46c76c
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2023-04-23 11:28:21 -0400

    Move all application code from 'class MainWindow'
    Too much code is tied to the gui framework. If you want to
    change gui framework you would have to rewrite all the
    code in MainWindow.
    
    Solution: Move all application specific code from
    MainWindow and put it in application_interface.py

--- application_interface.py
+++ application_interface.py
@@ -6,3 +6,48 @@ name = lambda: f'File "{__name__}.py",'
 line = lambda: f"line {str(sys._getframe(1).f_lineno)},"
 
 import sys
+from time import sleep, perf_counter
+import pickle
+import numpy as np
+import threading
+from pathlib import Path
+
+import spectrumAnalyzer as sa
+from spectrumAnalyzer import SA_Control as sa_ctl
+import serial_port as sp
+
+def freq_steps(startMHz, stopMHz, step_size, num_steps):
+        '''
+        Public method I want this function to get the frequencies and indexes
+        that will be used to slice the all_controls dictionary. It should be
+        based upon the width, or x range, of the plot window.
+        - First is to grab the start/stop/step from the user controls
+        - I can get the start and stop freqs from the plot window x range.
+        - Then I can access the indexes by feeding the freqs into the RFin_list
+          where the RFin_list is populated at prgram startup.
+        - Finally, populate the sweep control dictionary for this x range in
+          the Spectrum Analyzer module.
+        '''
+        # Frequencies:  Grab start/stop freqs and step size from the user controls
+        start_freq_mhz = round(startMHz, 3)
+        stop_freq_mhz = round(stopMHz, 3)
+        band_width = (stop_freq_mhz - start_freq_mhz) * 1000    # Convert MHz to kHz
+        ''' 401 prevents calibration from working '''
+        step_size_khz = int(band_width / num_steps)
+        num_steps_actual = int(band_width / step_size_khz)
+#        self.numFrequencySteps.setValue(num_steps_actual)   # Display the num_steps to the user
+#        self.intStepKHz.setValue(step_size_khz)
+        # Indexes:  At program start RFin_list doesn't exist so use the following
+        # method to get the indexes.
+        control_start_idx = int(start_freq_mhz * 1000)
+        control_stop_idx = int(stop_freq_mhz * 1000)
+        # Update the start/stop/step values in the Spectrum Analyzer module
+        sa_ctl.window_x_min = control_start_idx
+        sa_ctl.window_x_max = control_stop_idx
+        sa_ctl.window_x_range = sa_ctl.window_x_max - sa_ctl.window_x_min
+        # Get the start and stop indexes for the sweep frequencies list
+        start: int = sa_ctl().window_x_min
+        stop: int = sa_ctl().window_x_max
+        # Fill the list with new sweep frequecies
+        sa_ctl.swept_freq_list.clear()
+        return start, stop, step_size_khz, num_steps_actual   # Return to display the num_steps to the user
--- ui/mainWindow.py
+++ ui/mainWindow.py
@@ -40,7 +40,7 @@ from .Ui_mainWindow import Ui_MainWindow
 import spectrumAnalyzer as sa
 from spectrumAnalyzer import SA_Control as sa_ctl
 import serial_port as sp
-
+import application_interface as api
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     last_N = -1     # for monitoring the amount of data from serial_read()
@@ -213,40 +213,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         print(name(), line(), f'Calibration of {len(volts_list)} data points took {round(perf_counter()-start, 6)} seconds')
 
     def set_steps(self, num_steps: int = 401):
-        '''
-        Public method I want this function to get the frequencies and indexes
-        that will be used to slice the all_controls dictionary. It should be
-        based upon the width, or x range, of the plot window.
-        - First is to grab the start/stop/step from the user controls
-        - I can get the start and stop freqs from the plot window x range.
-        - Then I can access the indexes by feeding the freqs into the RFin_list
-          where the RFin_list is populated at prgram startup.
-        - Finally, populate the sweep control dictionary for this x range in
-          the Spectrum Analyzer module.
-        '''
-        # Frequencies:  Grab start/stop freqs and step size from the user controls
-        start_freq_mhz = round(self.floatStartMHz.value(), 3)
-        stop_freq_mhz = round(self.floatStopMHz.value(), 3)
-        band_width = (stop_freq_mhz - start_freq_mhz) * 1000    # Convert MHz to kHz step size
-        ''' 401 prevents calibration from working '''
-        step_size_khz = int(band_width / num_steps)
-        num_steps_actual = int(band_width / step_size_khz)
+        start_freq = round(self.floatStartMHz.value(), 3)
+        stop_freq = round(self.floatStopMHz.value(), 3)
+        step_size = self.intStepKHz.value()
+        start, stop, step, num_steps_actual = api.freq_steps(start_freq, stop_freq, step_size, num_steps)
         self.numFrequencySteps.setValue(num_steps_actual)   # Display the num_steps to the user
-        self.intStepKHz.setValue(step_size_khz)
-        # Indexes:  At program start RFin_list doesn't exist so use the following
-        # method to get the indexes.
-        control_start_idx = int(start_freq_mhz * 1000)
-        control_stop_idx = int(stop_freq_mhz * 1000)
-        # Update the start/stop/step values in the Spectrum Analyzer module
-        sa_ctl.window_x_min = control_start_idx
-        sa_ctl.window_x_max = control_stop_idx
-        sa_ctl.window_x_range = sa_ctl.window_x_max - sa_ctl.window_x_min
-        # Get the start and stop indexes for the sweep frequencies list
-        start: int = sa_ctl().window_x_min
-        stop: int = sa_ctl().window_x_max
-        step: int = self.intStepKHz.value()
-        # Fill the list with new sweep frequecies
-        sa_ctl.swept_freq_list.clear()
+        self.intStepKHz.setValue(step)
         if not self.PROGSTART:
             sa_ctl.swept_freq_list = self.get_swept_freq_list(start, stop, step)  # Way faster than np.arange()
 

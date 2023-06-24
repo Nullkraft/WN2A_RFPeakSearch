f4b48355931c566828147ade1bbccd042c948ccd
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2023-06-23 22:32:30 -0400

    Add DataLoader batching

--- zz_MHz_to_fmn.py
+++ zz_MHz_to_fmn.py
@@ -1,4 +1,5 @@
 import torch
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import sys
 
@@ -42,40 +43,67 @@ def MHz_to_fmn(target_freqs: torch.Tensor, M: torch.Tensor, Fpfd: torch.float16=
   best_F = F.gather(1, indices)         # indices is the index that results in best_F
   return best_F<<20 | best_M<<8 | N
 
+class FreqDataset(Dataset):
+    def __init__(self, start, end, step_size):
+        self.freqs = torch.arange(start, end, step_size)
+
+    def __len__(self):
+        return len(self.freqs)
+
+    def __getitem__(self, idx):
+        return self.freqs[idx]
+
 if __name__ == '__main__':
+  from time import perf_counter
   print()
 
   M = (torch.arange(2, 4096)).to(torch.int32)
-  num_steps = 66_000
-  num_steps_disp = 66_000
-  start = 4082.0
-  end = 4148.0
+  start = 0.0
+  end = 3000.0
+  num_steps = 10
+  num_steps = (end-start) / 0.001
+#  start = 4082.0
+#  end = 4148.0
   step_size = (end - start)/num_steps
   end = end + step_size/10
-
   steps = np.round(np.arange(start, end, step_size), decimals=3)
-  target_freqs_gpu = torch.arange(start, end, step_size)
+  print(line(), f'steps =\n{steps}')
 
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-  ret_gpu = MHz_to_fmn(target_freqs_gpu, M, device=device).cpu().numpy()
+  M = M.to(device)
 
-  freqs = [fmn_to_MHz(fmn) for fmn in ret_gpu]  # Verify results by converting back to frequency
-  freqs = np.round(np.array(freqs), decimals=3).flatten()
-  for i, freq in enumerate(freqs):
-    if not np.array_equal(steps[i], freq):
-      delta = round(abs((steps[i]-freq)), 3)
-      if delta > 0.007:
-        print(line(), f'Frequency step {i} = {steps[i]}\t\tfmn_to_MHz[{i}] = {freqs[i]}\t\t{delta = }')
+  all_fmn = []
+  batch_size = 16350    # This batch size causes 1.0 GB of video ram to be consumed
+  sweep_freqs = FreqDataset(start, end, step_size)
+  dataloader = DataLoader(sweep_freqs, batch_size=batch_size, shuffle=False, num_workers=0)
+  start_a = perf_counter()
+  for batch in dataloader:
+      target_freqs = batch.to(device)
+      fmn_batch = MHz_to_fmn(target_freqs, M, device=device).cpu().numpy()
+      all_fmn.append(fmn_batch)
+  print(line(), f'All batches took {round(perf_counter()-start_a, 6)} seconds')
+  ret_gpu = np.concatenate(all_fmn)   # all_fmn is a list of lists so cat them all together
+  print(line(), f'Max cuda memory = {round(torch.cuda.max_memory_allocated()/1024**3, 3)} GB')
+  
+  Fvcos = [fmn_to_MHz(fmn, Fpfd, show_fmn=False) for fmn in ret_gpu]  # Verify results by converting back to frequency
+  Fvcos = np.round(np.array(Fvcos), decimals=3).flatten()
 
-
-
-
-
-
-
-
-
-
+  deltas = []
+  for i, freq in enumerate(Fvcos):
+    Fvco = freq
+    while True:
+      if Fvco <= steps[i]:# + 0.0006:
+        break
+      Fvco = Fvco/2
+    if not np.array_equal(steps[i], Fvco):
+      delta = abs((steps[i]-Fvco))
+      if delta > 0.008:
+#        print(line(), f'Frequency step {i} = {steps[i]}\t\tfmn_to_MHz[{i}] = {Fvcos[i]}\t\t{delta = }')
+        deltas.append(delta)
+#      count += 1
+  
+  print(line(), f'Max delta = {max(deltas)}')
+#  print(line(), f'Num errors < = {max(lwr)}')
 
 
 

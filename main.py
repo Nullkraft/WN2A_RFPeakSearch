34f24b2fe620ba7215a814185cbb617df6df48a9
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2023-08-21 09:25:53 -0400

    main.py refactor tabs to 2 spaces

--- main.py
+++ main.py
@@ -25,25 +25,25 @@ line = lambda: f"line {str(sys._getframe(1).f_lineno)},"
 
 
 from ui.mainWindow import MainWindow
-from PyQt6 import QtWidgets             # requires 'pip install pyqtgraph'
+from PyQt6 import QtWidgets       # requires 'pip install pyqtgraph'
 import sys
 
 from time import perf_counter
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
-    start = perf_counter()
-    window = MainWindow()
-    print(name(), line(), f'INFO::Startup took {round(perf_counter()-start, 6)} seconds')
-    window.show()
-    sys.exit(app.exec())    # Using sys.exit() takes the return code from app.exec() and sends
-                            # it to the command line while app.exec() starts the event loop.
+  app = QtWidgets.QApplication(sys.argv)
+  start = perf_counter()
+  window = MainWindow()
+  print(name(), line(), f'INFO::Startup took {round(perf_counter()-start, 6)} seconds')
+  window.show()
+  sys.exit(app.exec())  # Using sys.exit() takes the return code from app.exec() and sends
+              # it to the command line while app.exec() starts the event loop.
 
-#    How To Reduce Coupling With Facade (Adding an abstraction layer)
-#    https://www.youtube.com/watch?v=jjoLejA4iAE
+#  How To Reduce Coupling With Facade (Adding an abstraction layer)
+#  https://www.youtube.com/watch?v=jjoLejA4iAE
 
 # Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
-    print()
-    main()
-#    print("Most lastest line ...")
+  print()
+  main()
+#  print("Most lastest line ...")

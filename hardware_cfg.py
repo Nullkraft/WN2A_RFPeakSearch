784fbe37c6c1264a20da66c54555d620a454d740
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2023-08-21 09:12:01 -0400

    Refactor tabs from 4 spaces to 2

--- hardware_cfg.py
+++ hardware_cfg.py
@@ -31,75 +31,75 @@ from enum import Enum, auto
 from functools import wraps
 
 class SPI_Device(Enum):
-    """ Tracks which of the SPI capable chips is selected """
-    ATTENUATOR = auto()
-    LO1 = auto()
-    LO2 = auto()
-    LO3 = auto()
+  """ Tracks which of the SPI capable chips is selected """
+  ATTENUATOR = auto()
+  LO1 = auto()
+  LO2 = auto()
+  LO3 = auto()
 
 
 @dataclass
 class Cfg():
-    max_error = 2**32
-    Vref = 2.595 #5.0                   # Controller ADC reference voltage
+  max_error = 2**32
+  Vref = 2.595 #5.0           # Controller ADC reference voltage
 
-    ref_clock_1 = 66.000
-    ref_clock_2 = 67.108864
-    ref_divider: int = 1                # R from the MAX2871 spec sheet
+  ref_clock_1 = 66.000
+  ref_clock_2 = 67.108864
+  ref_divider: int = 1        # R from the MAX2871 spec sheet
 
-    Fpfd1 = ref_clock_1 / ref_divider   # LO1 step size when using ref_clock 1
-    Fpfd2 = ref_clock_2 / ref_divider   # LO1 step size when using ref_clock 2
+  Fpfd1 = ref_clock_1 / ref_divider   # LO1 step size when using ref_clock 1
+  Fpfd2 = ref_clock_2 / ref_divider   # LO1 step size when using ref_clock 2
 
-    IF1: float = 3600.0                 # First intermediate frequency in MHz
-    IF2: float = 315.0                  # Second intermediate frequency in MHz
-    IF3: float = 45.0                   # Third intermediate frequency in MHz
+  IF1: float = 3600.0         # First intermediate frequency in MHz
+  IF2: float = 315.0          # Second intermediate frequency in MHz
+  IF3: float = 45.0           # Third intermediate frequency in MHz
 
-    RFin_max_freq = 3000                # No control codes available beyond this frequency
+  RFin_max_freq = 3000        # No control codes available beyond this frequency
 
 
 
 def memoize(func):
-    cache = {}
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        key = str(args) + str(kwargs)
-        if key not in cache:
-            cache[key] = func(*args, **kwargs)
-        return cache[key]
-    return wrapper
+  cache = {}
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+    key = str(args) + str(kwargs)
+    if key not in cache:
+      cache[key] = func(*args, **kwargs)
+    return cache[key]
+  return wrapper
 
 
 #@memoize
 @njit(nogil=True)
 def MHz_to_fmn(LO2_target_freq_MHz: float, ref_clock: float) -> int:
-    """ Form a 32 bit word containing F, M and N for the MAX2871.
-            Frac F is the fractional division value (0 to MOD-1)
-            Mod M is the modulus value
-            Int N is the 16-bit N counter value (In Frac-N mode min 19 to 4091)
-    """
-    R = 1
-    max_error = 2**32
-    for div_range in range(8):
-        div = 2**div_range
-        Fvco = div * LO2_target_freq_MHz
-        if Fvco >= 3000:                    # vco fundamental freq = 3000 MHz (numba requires a constant?)
-            break
-    Fpfd = ref_clock / R
-    N = int(Fvco / Fpfd)
-    Fract = round(Fvco / Fpfd - N, 9)
-    for M in list(range(4095,1,-1)):
-        F = round(Fract * M)
-        Err1 = abs(Fvco - (Fpfd * (N + F/M)))
-        if Err1 == 0:
-            best_F = F
-            best_M = M
-            break
-        if Err1 <= max_error:   # <= selects highest value of Best_M, more accurate target freq
-            max_error = Err1
-            best_F = F
-            best_M = M
-#    print(name(), line(), f'F = {best_F} : M = {best_M} : N = {N}')
-    return best_F<<20 | best_M<<8 | N
+  """ Form a 32 bit word containing F, M and N for the MAX2871.
+      Frac F is the fractional division value (0 to MOD-1)
+      Mod M is the modulus value
+      Int N is the 16-bit N counter value (In Frac-N mode min 19 to 4091)
+  """
+  R = 1
+  max_error = 2**32
+  for div_range in range(8):
+    div = 2**div_range
+    Fvco = div * LO2_target_freq_MHz
+    if Fvco >= 3000:          # vco fundamental freq = 3000 MHz (numba requires a constant?)
+      break
+  Fpfd = ref_clock / R
+  N = int(Fvco / Fpfd)
+  Fract = round(Fvco / Fpfd - N, 9)
+  for M in list(range(4095,1,-1)):
+    F = round(Fract * M)
+    Err1 = abs(Fvco - (Fpfd * (N + F/M)))
+    if Err1 == 0:
+      best_F = F
+      best_M = M
+      break
+    if Err1 <= max_error:   # <= selects highest value of Best_M, more accurate target freq
+      max_error = Err1
+      best_F = F
+      best_M = M
+#  print(name(), line(), f'F = {best_F} : M = {best_M} : N = {N}')
+  return best_F<<20 | best_M<<8 | N
 
 
 
